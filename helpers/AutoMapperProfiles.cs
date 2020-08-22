7e76376a7bbfd5a45ceaacd9689b0fc73f586421
Laurie Roy <laurieroy.dev@gmail.com>
2020-08-22 10:47:54 -0600

    Customize config to set main photo url

--- helpers/AutoMapperProfiles.cs
+++ helpers/AutoMapperProfiles.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using AutoMapper;
 using DatingApp.API.DTOs;
 using DatingApp.API.Models;
@@ -8,8 +9,12 @@ namespace DatingApp.API.helpers
     {
         public AutoMapperProfiles()
         {
-            CreateMap<User, UserForListDto>();
-            CreateMap<User, UserForDetailsDto>();
+            CreateMap<User, UserForListDto>()
+                .ForMember(dest => dest.PhotoUrl, opt => 
+                    opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url));
+            CreateMap<User, UserForDetailsDto>()
+                .ForMember(dest => dest.PhotoUrl, opt => 
+                    opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url));
             CreateMap<Photo, PhotosForDetailedDto>();
         }
     }
