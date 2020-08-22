34989866bc923230115e37bdd24f90f7802f1845
Laurie Roy <laurieroy.dev@gmail.com>
2020-08-22 11:03:16 -0600

    Add CalculateAge helper; use to configure age

--- helpers/AutoMapperProfiles.cs
+++ helpers/AutoMapperProfiles.cs
@@ -11,10 +11,14 @@ namespace DatingApp.API.helpers
         {
             CreateMap<User, UserForListDto>()
                 .ForMember(dest => dest.PhotoUrl, opt => 
-                    opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url));
+                    opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url))
+                .ForMember(dest => dest.Age, opt => 
+                    opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
             CreateMap<User, UserForDetailsDto>()
                 .ForMember(dest => dest.PhotoUrl, opt => 
-                    opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url));
+                    opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url))
+                .ForMember(dest => dest.Age, opt => 
+                    opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
             CreateMap<Photo, PhotosForDetailedDto>();
         }
     }
