a1c54ef754061f03094061f7175b4915a0225194
Laurie Roy <laurieroy.dev@gmail.com>
2020-08-29 10:29:15 -0600

    Filter For Age

--- helpers/UserParams.cs
+++ helpers/UserParams.cs
@@ -14,5 +14,7 @@ namespace DatingApp.API.helpers
         public int UserId { get; set; }
         public string Gender { get; set; }
         
+        public int MinAge { get; set; } = 18;
+        public int MaxAge { get; set; } = 99;
     }
 }
